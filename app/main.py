8d440ed7b61391ee90dc7ddcd0136e87782ada3c
Eng. Sadat Hasakya <hersacemusasadat@gmail.com>
2026-04-16 10:33:08 +0300

    feat: add database, models, schemas and main app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from .database import engine, Base, SessionLocal
+from . import models, schemas
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "API is running successfully"}
+Base.metadata.create_all(bind=engine)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/users/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(name=user.name, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get("/users/", response_model=list[schemas.UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    return db.query(models.User).all()
